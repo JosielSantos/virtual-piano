14b33839d2b13634779e53a8b8a8f073005b4856
Josiel Santos <josiel.lkp@gmail.com>
2021-10-10 18:57:30 -0300

    Ajustado MIDI pygame driver

--- src/midi/driver_pygame.py
+++ src/midi/driver_pygame.py
@@ -1,25 +1,26 @@
 import pygame.midi
 
 from midi._abstracts import AbstractMidiOutput
+from midi import _constants
 
 class PygameMidiOutput(AbstractMidiOutput):
     def __init__(self, output_id = None):
         pygame.midi.init()
         if output_id is None:
             output_id = pygame.midi.get_default_output_id()
-        self.__handler = pygame.midi.Output(output_id, 0)
+        self.__pygame = pygame.midi.Output(output_id, 0)
 
     def __del__(self):
         pygame.midi.quit()
 
     def note_on(self, note, volume = 127, channel_number = 0):
-        self.__handler.note_on(note, volume, channel_number)
+        self.__pygame.note_on(note, volume, channel_number)
 
     def note_off(self, note, channel_number = 0):
-        self.__handler.note_off(note, 0, channel_number)
+        self.__pygame.note_off(note, 0, channel_number)
 
     def set_instrument(self, instrument_id, channel_number = 0):
-        self.__handler.set_instrument(instrument_id, channel_number)
+        self.__pygame.set_instrument(instrument_id, channel_number)
 
     def control_change(self, control, value, channel_number):
-        self.__handler.write_short(0xB0 + channel_number, control, value)
+        self.__pygame.write_short(_constants.MIDI_MESSAGE_CONTROL_CHANGE + channel_number, control, value)
