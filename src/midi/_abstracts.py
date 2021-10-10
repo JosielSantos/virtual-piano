a691ab0ab8de1ed881879df21721ed51cde7b8d7
Josiel Santos <josiel.lkp@gmail.com>
2021-10-10 19:22:20 -0300

    Modificada classe abstrata AbstractMidiOutput para usar o módulo ABC

--- src/midi/_abstracts.py
+++ src/midi/_abstracts.py
@@ -1,17 +1,23 @@
+from abc import ABC, abstractmethod
+
 from midi import _constants
 
-class AbstractMidiOutput:
+class AbstractMidiOutput(ABC):
+    @abstractmethod
     def note_on(self, note, volume = 127, channel_number = 0):
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def note_off(self, note, channel_number = 0):
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def set_instrument(self, instrument_id, channel_number = 0):
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def control_change(self, control, value, channel_number):
-        raise NotImplementedError()
+        pass
 
     def set_channel_volume(self, volume, channel_number = 0):
         self.control_change(_constants.CONTROL_CHANNEL_VOLUME , volume, channel_number)
