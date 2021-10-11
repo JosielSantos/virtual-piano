1b87952530c32d78956ec2c5de4203468bcf753d
Josiel Santos <josiel.lkp@gmail.com>
2021-10-11 07:44:17 -0300

    Fix: Importar os MIDI drivers nas suas respectivas funções fábrica (importar só quando for utilizar)

--- src/midi_factory.py
+++ src/midi_factory.py
@@ -1,10 +1,11 @@
 import constants
-from midi import driver_fluidsynth, driver_pygame
 
 class MidiOutputFactory:
     def factory_pygame(self, output_id):
+        from midi import driver_pygame
         output_id = None if output_id == constants.MIDI_OUTPUT_DEFAULT_DRIVER else output_id
         return driver_pygame.PygameMidiOutput(output_id)
 
     def factory_fluidsynth(self, soundfont_filepath):
+        from midi import driver_fluidsynth
         return driver_fluidsynth.FluidSynthMidiOutput(soundfont_filepath)
